fcb2c78e7ee88674fe9d7e33dabd80baafd20b6e
Omar <omarjarkas1997@hotmail.com>
2024-08-21 17:18:42 +1200

    label changes (not working)

--- swarm-deploy.sh
+++ swarm-deploy.sh
@@ -2,15 +2,15 @@
 
 # Set the IP addresses of the admin, managers, and workers nodes
 admin=192.168.3.5
-manager1=3.85.133.36
-worker1=54.80.8.228
-worker2=54.152.246.228
-worker3=54.211.103.39
+mainManagerNode=3.85.133.36
+verifierNode=54.80.8.228
+registrarNode=54.152.246.228
+agent1Node=54.211.103.39
 
 # Set the workers' hostnames
-workerHostname1=verifier-node
-workerHostname2=registrar-node
-workerHostname3=agent-1-node
+verifierHostname=verifier-node
+registrarHostname=registrar-node
+agent1Hostname=agent-node
 
 # User of remote machines
 user=ubuntu
@@ -19,15 +19,17 @@ user=ubuntu
 interface=eth0
 
 # Array of extra managers
+additionnal_managers=false
 extra_managers=()
 
 # Array of workers
-workers=($worker1 $worker2 $worker3)
-agentWorkers=($worker3)
-workerHostnames=($workerHostname1 $workerHostname2 $workerHostname3)
+workers=($verifierNode $registrarNode $agent1Node)
+workersHostnames=($verifierHostname $registrarHostname $agent1Hostname)
+agentWorkers=($agent1Node)
+agentsHostnames=($agent1Hostname)
 
 # Array of all
-all=($manager1 $worker1 $worker2 $worker3)
+all=($mainManagerNode $verifierNode $registrarNode $agent1Node)
 
 #ssh certificate name variable
 certName=omar-vm-priv-key.pem
@@ -38,8 +40,7 @@ certName=omar-vm-priv-key.pem
 
 # Move SSH certs to ~/.ssh and change permissions
 cp /home/$user/{$certName,$certName.pub} /home/$user/.ssh
-chmod 600 /home/$user/.ssh/$certName 
-# chmod 644 /home/$user/.ssh/$certName.pub
+chmod 600 /home/$user/.ssh/$certName
 
 # Create SSH Config file to ignore checking (don't use in production!)
 echo "StrictHostKeyChecking no" > ~/.ssh/config
@@ -50,8 +51,8 @@ echo "StrictHostKeyChecking no" > ~/.ssh/config
 # done
 # 
 # Copy SSH keys to MN1 to copy tokens back later
-scp -i /home/$user/.ssh/$certName /home/$user/$certName $user@$manager1:~/.ssh
-# scp -i /home/$user/.ssh/$certName /home/$user/$certName.pub $user@$manager1:~/.ssh
+scp -i /home/$user/.ssh/$certName /home/$user/$certName $user@$mainManagerNode:~/.ssh
+# scp -i /home/$user/.ssh/$certName /home/$user/$certName.pub $user@$mainManagerNode:~/.ssh
 
 # 
 # Install dependencies for each node (Docker)
@@ -77,14 +78,14 @@ for newnode in "${all[@]}"; do
   apt-get update
   apt-get install git
   exit
-EOF
-  echo -e " \033[32;5m$newnode - Docker & GlusterFS installed!\033[0m"
+  EOF
+  echo -e " \033[32;5m$newnode - Docker installed!\033[0m"
 done
 
 # Step 1: Create Swarm on first node
 # Debug purpose : scp permet de copier de maniere securisee des fichier entre deux entites, ici les token sont copies
-ssh -tt $user@$manager1 -i ~/.ssh/$certName sudo su <<EOF
-docker swarm init --advertise-addr $manager1 --default-addr-pool 10.20.0.0/16 --default-addr-pool-mask-length 26
+ssh -tt $user@$mainManagerNode -i ~/.ssh/$certName sudo su <<EOF
+docker swarm init --advertise-addr $mainManagerNode --default-addr-pool 10.20.0.0/16 --default-addr-pool-mask-length 26
 docker swarm join-token manager | grep -Po 'docker swarm join --token \K[^\s]*' > manager.txt
 docker swarm join-token worker | grep -Po 'docker swarm join --token \K[^\s]*' > worker.txt
 echo "StrictHostKeyChecking no" > ~/.ssh/config
@@ -93,48 +94,31 @@ scp -i /home/$user/.ssh/$certName /home/$user/manager.txt $user@$admin:~/manager
 scp -i /home/$user/.ssh/$certName /home/$user/worker.txt $user@$admin:~/worker
 exit
 EOF
-echo -e " \033[32;5mManager1 Completed\033[0m"
+echo -e " \033[32;5mmainManagerNode Completed\033[0m"
 
 # Step 2: Set variables
 managerToken=`cat manager`
 workerToken=`cat worker`
 
 # Step 3: Connect additional manager
-for newnode in "${extra_managers[@]}"; do
+if [[ $additionnal_managers == true ]]; then
+  for newnode in "${extra_managers[@]}"; do
+    ssh -tt -i ~/.ssh/$certName "$user@$newnode" sudo bash <<EOF
+    docker swarm join --token $managerToken $mainManagerNode
+    exit
+    EOF
+    echo -e " \033[32;5m$newnode - Manager node joined successfully!\033[0m"
+  done
+fi
+
+# Step 4: Connect additional workers
+for i in "${!workers[@]}"; do
+  newnode=${workers[$i]}
+  workerHostname=${workersHostnames[$i]}
   ssh -tt $user@$newnode -i ~/.ssh/$certName sudo su <<EOF
-  docker swarm join \
-  --token  $managerToken \
-  $manager1
+  sudo hostnamectl set-hostname $workerHostname
+  docker swarm join --token $workerToken $mainManagerNode
   exit
-EOF
-  echo -e " \033[32;5m$newnode - Manager node joined successfully!\033[0m"
+  EOF
+  echo -e " \033[32;5m$newnode - Worker node joined successfully with hostname $workerHostname!\033[0m"
 done
-
-# Step 4: Connect additional worker
-for newnode in "${workers[@]}"; do
-  ssh -tt $user@$newnode -i ~/.ssh/$certName sudo su <<EOF
-  docker swarm join \
-  --token  $workerToken \
-  $manager1
-  docker node update --label-add worker=true ${workerHostname[@]}
-  exit
-EOF
-  echo -e " \033[32;5m$newnode - Worker node joined successfully!\033[0m"
-done
-
-# Step 5: Label workers from the manager node
-ssh -tt $user@$manager1 -i $pathToCert/$certName sudo su <<EOF
-for hostname in ${workerHostnames[@]}; do
-  docker node update --label-add worker=true \$hostname
-done
-exit
-EOF
-
-
-# Step 6 : Connect to all machines
-# TO COMPLETE
-# for newnode in "${all[@]}"; do
-#   ssh $user@$newnode -i ~/.ssh/$certName sudo su <<EOF
-  
-# EOF
-# done
